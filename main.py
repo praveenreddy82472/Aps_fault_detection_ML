83c499a63bdf33bbaeb3e1f6b7254e0bd21cdb2b
PraveenReddy <tumatipraveenreddy23@gmail.com>
2024-02-15 16:49:05 -0600

    DataIngestion added

---
+++ main.py
@@ -0,0 +1,18 @@
+import os,sys
+from sensor.utils import *
+from sensor.logger import logging
+from sensor.entity import config_entity
+from sensor.entity.config_entity import *
+from sensor.components import data_ingestion
+from sensor.components.data_ingestion import DataIngestion
+
+
+if __name__ == "__main__":
+    try:
+        training_pipeline_config = config_entity.TrainingPipelineConfig()
+        data_ingestion_config = DataIngestionConfig(training_pipeline_config=training_pipeline_config)
+        print(data_ingestion_config.to_dict())
+        data_ingestion = DataIngestion(data_ingestion_config=data_ingestion_config)
+        print(data_ingestion.initiate_data_ingestion())
+    except Exception as e:
+        print(e)
