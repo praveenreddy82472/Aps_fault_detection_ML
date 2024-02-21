f62e3eeaacef84442b8d3c5d2bddceebabba7326
PraveenReddy <tumatipraveenreddy23@gmail.com>
2024-02-20 18:04:08 -0600

    model trainer done

--- sensor/entity/artifact_entity.py
+++ sensor/entity/artifact_entity.py
@@ -17,7 +17,11 @@ class DataTransformationArtifact:
     transfrom_test_path:str
     target_encoder_path:str
     
+@dataclass
+class ModelTrainerArtifact:
+    model_path:str
+    f1_train_score:float
+    f1_test_score:float
     
-class ModelTrainigArtifact:...
 class ModelEvaluationArtifact:...
 class ModelPusherArtifact:...
