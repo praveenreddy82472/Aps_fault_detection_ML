982ca5dd5ce5a6d17b48e1ddbedd4dd94dd343c8
PraveenReddy <tumatipraveenreddy23@gmail.com>
2024-02-23 12:34:27 -0600

    model_evaluation done

--- main.py
+++ main.py
@@ -16,35 +16,39 @@ if __name__ == "__main__":
         training_pipeline_config = config_entity.TrainingPipelineConfig()
         
         #Data Ingestion
+        logging.info("Ingestion Executed")
         data_ingestion_config = DataIngestionConfig(training_pipeline_config=training_pipeline_config)
         print(data_ingestion_config.to_dict())
         data_ingestion = DataIngestion(data_ingestion_config=data_ingestion_config)
         data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
         
         #Data Validation
-        
+        logging.info("validation Executed")
         data_validation_config = DataValidationConfig(training_pipeline_config=training_pipeline_config)
         data_validation = DataValidation(data_validation_congif=data_validation_config,data_ingestion_artifact=data_ingestion_artifact)
         data_validation_artifact = data_validation.initiate_data_validation()
         print(data_validation_artifact)
-        """
+        
         #Data Transformation
+        logging.info("Transformation Executed")
         data_transformation_config = config_entity.DataTransformationConfig(training_pipeline_config=training_pipeline_config)
         data_transformation = DataTransformation(data_transformation_config=data_transformation_config,data_ingestion_artifact=data_ingestion_artifact)
         data_transformation_artifact  = data_transformation.initiate_data_transformation()
         print(data_transformation_artifact)
         
         #Model Trainer
+        logging.info("Trainer Executed")
         model_trainer_config= config_entity.ModelTrainerConfig(training_pipeline_config=training_pipeline_config)
         model_trainer = ModelTrainer(model_trainer_config=model_trainer_config,data_transformation_artifact=data_transformation_artifact)
         model_trainer_artifact= model_trainer.initiate_model_trainer()
         
         #model Evaluation
+        logging.info("Evaluation Executed")
         model_eval_config = config_entity.ModelEvaluationConfig(training_pipeline_config=training_pipeline_config)
         model_eval = ModelEvaluation(model_eval_config=model_eval_config,data_ingestion_artifact=data_ingestion_artifact,data_transformation_artifact=data_transformation_artifact,model_trainer_artifact=model_trainer_artifact)
         model_eval_artifact = model_eval.initiate_model_evaluation()
         
-        """
+        
     except Exception as e:
         print(e)
 
