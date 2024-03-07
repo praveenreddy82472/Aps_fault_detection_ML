9df557f80bcf9c8b6c9bd5d5e0dc3b1a00bf9f6b
Praveen Reddy <65601696+praveenreddy82472@users.noreply.github.com>
2024-03-07 13:07:01 -0600

    Update training_pipeline.py

--- airflow/dags/training_pipeline.py
+++ airflow/dags/training_pipeline.py
@@ -13,7 +13,7 @@ with DAG(
     # [END default_args]
     description='Sensor Fault Detection',
     schedule_interval="@weekly",
-    start_date=pendulum.datetime(2024, 3, 5, tz="UTC"),
+    start_date=pendulum.datetime(2024, 3, 7, tz="UTC"),
     catchup=False,
     tags=['example'],
 ) as dag:
